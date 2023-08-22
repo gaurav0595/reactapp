5d2248773695bc2d8013a555f38dd64f4c00fd07
root <root@gaurav.k@sarv.com>
2023-08-22 16:57:34 +0000

    fsdfjdsfj

---
+++ build/precache-manifest.99e27dd16900a614c53acff4cfc5d223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d795c9018c454dfdfeb49223574e94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1739ba9b262b3dfa66cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23062690b11357f493a7",
+    "url": "/static/js/2.de36018d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.de36018d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1739ba9b262b3dfa66cf",
+    "url": "/static/js/main.1fbdca48.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
